5da469120a1ec6d81d4a27cce241dea96c2c9d0e
Somtochukwu Uchegbu <somtochukwuuchegbu@gmail.com>
2020-09-19 10:55:30 +0100

    made changes to the urls.py file

--- e_learning_site/urls.py
+++ e_learning_site/urls.py
@@ -19,6 +19,7 @@ from users.views import register_view
 from courses.views import home_view
 from django.conf import settings 
 from django.conf.urls.static import static 
+from django.contrib.auth import views
 
 
 urlpatterns = [
